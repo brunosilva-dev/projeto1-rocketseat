2daaa1f9ce268dbe4f7e3e079eb97496eab273f2
Bruno Silva <silva.dev@hotmail.com>
2024-12-26 14:46:21 -0300

    Subindo app feito em aula rocketseat (projeto IMHERE)

--- App.tsx
+++ App.tsx
@@ -1,19 +1,7 @@
-import { Text, View } from "react-native";
+import { Home } from "./src/screens/Home"
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#24BFA5",
-        padding: 24,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text style={{ color: "#fdfcfe", fontSize: 24, fontWeight: "bold" }}>
-        Bem-vindo ao New Meca!
-      </Text>
-    </View>
+    <Home />
   );
 }
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+
+import { styles } from "./styles";
+
+function handleParticipantAdd() {
+  console.log("Clicou no botão de adicionar!")
+}
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}> + </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
